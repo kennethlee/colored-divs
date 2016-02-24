a9e9eb0e32ed027e411578b9e4dcff5c14986860
Kenneth Lee <kennyxlee@gmail.com>
2016-02-24 15:34:11 -0800

    Make 'smoketest' more obvious

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('VERILY, THOU ART READY TO ROCK.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>
+  <h3>VERILY, THOU ART READY TO ROCK.</h3>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
